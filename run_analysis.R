5361d572afe048cefba2e256bfa7d6c58ff2ae1a
nostatisfaction <nostatisfaction@gmail.com>
2015-05-23 23:59:21 +0100

    Committing initial files

---
+++ run_analysis.R
@@ -0,0 +1,28 @@
+
+
+# Read in the training and test data sets ---------------------------------
+
+
+
+# Merge the training and the test sets to create one data set -------------
+# Merges the training and the test sets to create one data set.
+
+
+# Extract the measurements on the mean and sd for each measurement --------
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+
+# Use descriptive activity names to name the activities in the dat --------
+# Uses descriptive activity names to name the activities in the data set
+
+
+# Label the data set with descriptive variable names ----------------------
+# Appropriately labels the data set with descriptive variable names. 
+
+
+
+
+# Create a tidy data set containing averages ------------------------------
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
